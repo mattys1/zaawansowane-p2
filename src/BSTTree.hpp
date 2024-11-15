ed3cd10d6d56da9b1076a8ac97c38c6c7813d44c
mattys1 <mattys0082@gmail.com>
2024-11-15 13:40:09 +0100

    nic nie dziala

--- src/BSTTree.hpp
+++ src/BSTTree.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ranges>
+#include <vector>
 template <typename T>
 class BSTTree {
 private:
@@ -26,6 +28,11 @@ private:
 	void recursive_add(const T& element, Tree* node, Tree* parentNode = nullptr) {
 		if(node == nullptr) {
 			node = new Tree(element);
+
+			if(parentNode == nullptr) {
+				return;
+			}
+
 			node->parent = parentNode;
 
 			if(node->contents >= parentNode->contents) {
@@ -44,15 +51,104 @@ private:
 		}	
 	}
 
+	void preorder_traverse_recursive(Tree* node, std::vector<Tree*>& traversedTrees) {
+		if(node == nullptr) {
+			return;
+		}
+
+		traversedTrees.push_back(node);
+
+		preorder_traverse_recursive(node->left, traversedTrees);
+		preorder_traverse_recursive(node->right, traversedTrees);
+	}
+
+	void inorder_traverse_recursive(Tree* node, std::vector<Tree*>& traversedTrees) {
+		if(node == nullptr) {
+			return;
+		}
+
+		inorder_traverse_recursive(node->left, traversedTrees);
+
+		traversedTrees.push_back(node);
+
+		inorder_traverse_recursive(node->right, traversedTrees);
+	}
+
+	void postorder_traverse_recursive(Tree* node, std::vector<Tree*>& traversedTrees) {
+		if(node == nullptr) {
+			return;
+		}
+
+		postorder_traverse_recursive(node->left, traversedTrees);
+		postorder_traverse_recursive(node->right, traversedTrees);
+
+		traversedTrees.push_back(node);
+	}
+
 public:
 	BSTTree(const T& element): root { new Tree(element) } {}
 	BSTTree(void): root { nullptr } {}
 	~BSTTree() { delete root; }
 
 	/// Add a single element to the tree
+	/// @param element element of T to insert to the tree
 	///
 	/// This operation happens recursively
 	void add(const T& element) {
 		recursive_add(element, root);
+	}
+
+	void delete_tree(void) {
+		delete root;
+		root = nullptr;
+	}
+
+	/// Traverse the tree in the preorder direction and return a vector the contents of each node
+	/// @return preordered vector of the elements of the tree
+	std::vector<T> traverse_preorder(void) {
+		std::vector<Tree*> traversedTrees;
+	
+		preorder_traverse_recursive(root, traversedTrees);
+	
+		return traversedTrees | std::ranges::views::transform([](const Tree* tree) { return tree->contents; }) | std::ranges::to<std::vector>(); 
+	}
+
+	/// Traverse the tree in the inorder direction and return a vector the contents of each node
+	/// @return inordered vector of the elements of the tree
+	std::vector<T> traverse_inorder() {
+		std::vector<Tree*> traversedTrees;
+
+		inorder_traverse_recursive(root, traversedTrees);
+
+		return traversedTrees | std::ranges::views::transform([](const Tree* tree) { return tree->contents; }) | std::ranges::to<std::vector>(); 
+	}
+
+	/// Traverse the tree in the postorder direction and return a vector the contents of each node
+	/// @return postordered vector of the elements of the tree
+	std::vector<T> traverse_postorder() {
+		std::vector<Tree*> traversedTrees;
+
+		postorder_traverse_recursive(root, traversedTrees);
+
+		return traversedTrees | std::ranges::views::transform([](const Tree* tree) { return tree->contents; }) | std::ranges::to<std::vector>(); 
+	}
+
+	/// Delete an element of a given value
+	/// @param value The value of an element that is to be deleted.
+	///
+	/// This does not differentiate between unique elements of the same value
+	int delete_element(T value) {
+		std::vector<Tree*> traversedTrees;
+		inorder_traverse_recursive(root, traversedTrees);
+		auto elemeOfValue = std::ranges::find_if(traversedTrees.begin(), traversedTrees.end(), 
+										   [&value](const Tree* tree) { return tree->contents == value;});
+
+		if(elemeOfValue == traversedTrees.end()) {
+			return -1;
+		}
+
+		auto successor = std::next(elemeOfValue);
+
+		return 0;
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "BSTTree.hpp"
+#include <print>
 
 int main (int argc, char *argv[]) {
 	BSTTree<int> tree(5);
@@ -9,6 +10,33 @@ int main (int argc, char *argv[]) {
 	tree.add(10);
 	tree.add(7);
 	tree.add(1);
+
+	std::println("Preorder traversal:");
+	for(const auto item : tree.traverse_preorder()) {
+		std::print("{},", item);
+	}
+
+	std::println();
+
+	std::println("Inorder traversal:");
+	for(const auto item : tree.traverse_inorder()) {
+		std::print("{},", item);
+	}
+
+	std::println();
+
+	std::println("Postorder: traversal:");
+	for(const auto item : tree.traverse_postorder()) {
+		std::print("{},", item);
+	}
+
+	tree.delete_element(1);
+
+	tree.delete_tree();
+
+	tree.add(8);
+	tree.add(2);
+	tree.add(9);
 
 	return 0;
 }
